0956f9d74cd56ca58782f1a4a82b953acb984120
Christine Bumpous <bumpouce@gmail.com>
2020-03-11 14:03:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :total
+
+  def initialize (*employee)
+    @total = 0
+  end
+  
+  def discount
+    20
+  end 
+
+
+  def total
+    100
+  end
+
+  
+  def add_item (title, price, *quantity)
+    @total = total + price
+  end
+  
+
+end
